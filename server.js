b723a4d84114d87de03c82f8dd1d790ee113a169
AghribAbdessalem <aghribabdessalem@gmail.com>
2020-06-14 21:48:41 +0100

    Initial author routes

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+var bodyParser = require('body-parser')
 
 app.set('view engine' , 'ejs')
 app.set('views' , __dirname + '/views')
 app.set('layout' , 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit : '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 
@@ -22,6 +25,7 @@ db.once('open' , () => console.log('Connected to Mongoose'))
 
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
